d42de609ea5fbf7a8cf00ab3fc5730144b0656c1
Sohail Khan <39166458+SohailKhanPAK@users.noreply.github.com>
2020-07-17 23:10:03 +0500

    TO DO APP
    Using HTML ,CSSS JAVA SCRIPT

---
+++ app.js
@@ -0,0 +1,67 @@
+var colors =['green','royalblue','teal','violet','tomato']
+var date = document.getElementById('date')
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+day= days[d.getDay()];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+month = months[d.getMonth()];
+dau_digit= d.getDate();
+
+date.innerHTML=`${day} , ${month} ${dau_digit}`
+
+var content = document.getElementById('content')
+var textField = document.getElementById('textField')
+var n =0;
+function toDo(){
+   if (textField.value!=""){
+content.innerHTML+=`
+
+<div class="row content1 ${colors[n]}">
+
+    <div class="col-2 col-sm-1 mt-3 circle-icon">
+        <i class="fa fa-circle-thin fa-big" aria-hidden="true"></i>
+    </div>
+
+    <div class="col-8 col-sm-10 mt-3   text">
+        <p id="text">${textField.value}</p>
+    </div>
+
+    <div class="col-2 col-sm-1 mt-3   trash-icon">
+        <i class="fa fa-trash-o fa-big"  aria-hidden="true"></i>
+    </div>
+</div>
+
+`
+
+textField.value=""
+n++
+if (n>5) {
+  n=0
+}
+
+   }
+}
+
+
+refresh = () =>{
+
+    content.innerHTML=""
+}
+// var check = document.getElementById('check')
+// var text = document.getElementById('text')
+// var flag =  check.childNodes[1]
+// console.log(flag.hasAttribute('aria-disabled'))
+// checked = () =>{
+//     if (flag.hasAttribute('aria-disabled') !=true){
+//     check.innerHTML=`<i class="fa fa-check-circle-o icon-color fa-big" aria-disabled="false" aria-hidden="true"></i>`
+//     text.innerHTML =  `<del>${text.innerHTML}</del>`
+//     }
+//     else if ((flag.hasAttribute('aria-disabled') ==true)) {
+//         check.innerHTML=`<i  class="fa fa-circle-thin fa-big" aria-hidden="true"></i>  `
+//         text.innerHTML =  text.innerHTML
+//         console.log('true')
+//     }
+
+
+
+// }
